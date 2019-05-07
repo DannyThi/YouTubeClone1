391127e5e38dbc9084b686e4402d841319354029
Thi Danny <Danny@Danny-This-MacBook-Pro.local>
2019-05-07 21:32:38 +0900

    Removed storyboard and added code to load views programatically.

---
+++ YouTubeClone/HomeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  YouTubeClone
+//
+//  Created by Thi Danny on 2019/05/07.
+//  Copyright © 2019 Spawn Camping Panda. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Home"
+        
+        view.backgroundColor = UIColor.white
+    }
+
+
+}
+
