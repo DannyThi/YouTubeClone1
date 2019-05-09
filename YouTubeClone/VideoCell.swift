e8de32b46ae2712eb6c916578f60cf0aaf51436a
Thi Danny <Danny@Danny-This-MacBook-Pro.local>
2019-05-09 14:08:17 +0900

    Added more subviews and constraints.

--- YouTubeClone/VideoCell.swift
+++ YouTubeClone/VideoCell.swift
@@ -16,34 +16,88 @@ class VideoCell: UICollectionViewCell {
         return view
     }()
     
+    let userProfileImageView: UIImageView = {
+        let view = UIImageView()
+        view.backgroundColor = UIColor.green
+        view.layer.cornerRadius = view.bounds.width / 2
+        view.clipsToBounds = false
+        return view
+    }()
+    
+    let titleLabel: UILabel = {
+        let view = UILabel()
+        //view.backgroundColor = UIColor.purple
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.text = "Title goes here"
+        return view
+    }()
+    
+    let subtitleTextView: UITextView = {
+        let view = UITextView()
+        //view.backgroundColor = UIColor.red
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.text = "Subtitle text goes here."
+        return view
+    }()
     
     let seperatorView: UIView = {
         let view = UIView()
         view.backgroundColor = UIColor.black
+        
         return view
     }()
     
-    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
         setupView()
     }
 
     func setupView() {
         // We need to add the views before we can apply constraints.
         addSubview(thumbnailImageView)
+        addSubview(userProfileImageView)
+        addSubview(titleLabel)
+        addSubview(subtitleTextView)
         addSubview(seperatorView)
 
         // Thumbnail Image View
         addConstraints(withVisualFormat: "H:|-16-[v0]-16-|", views: thumbnailImageView)
         
+        // User Profile Image View
+        addConstraints(withVisualFormat: "H:|-16-[v0(44)]", views: userProfileImageView)
+        
+        // Title Label
+        addConstraint(NSLayoutConstraint(item: titleLabel, attribute: .top, relatedBy: .equal,
+                                         toItem: userProfileImageView, attribute: .top,
+                                         multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: titleLabel, attribute: .leading, relatedBy: .equal,
+                                         toItem: userProfileImageView, attribute: .trailing,
+                                         multiplier: 1, constant: 8))
+        
+        addConstraint(NSLayoutConstraint(item: titleLabel, attribute: .trailing, relatedBy: .equal,
+                                         toItem: contentView, attribute: .trailing,
+                                         multiplier: 1, constant: -16))
+        
+        // Subtitle Text View
+        addConstraint(NSLayoutConstraint(item: subtitleTextView, attribute: .top, relatedBy: .equal,
+                                         toItem: titleLabel, attribute: .bottom,
+                                         multiplier: 1, constant: 8))
+        addConstraint(NSLayoutConstraint(item: subtitleTextView, attribute: .leading, relatedBy: .equal,
+                                         toItem: titleLabel, attribute: .leading,
+                                         multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: subtitleTextView, attribute: .trailing, relatedBy: .equal,
+                                         toItem: titleLabel, attribute: .trailing,
+                                         multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: subtitleTextView, attribute: .height, relatedBy: .equal,
+                                         toItem: contentView, attribute: .height,
+                                         multiplier: 0, constant: 20))
+        
         // Seperator View
         addConstraints(withVisualFormat: "H:|[v0]|", views: seperatorView)
 
-        
         // Vertical Spacing
-        addConstraints(withVisualFormat: "V:|-16-[v0]-16-[v1(1)]|", views: thumbnailImageView, seperatorView)
+        addConstraints(withVisualFormat: "V:|-16-[v0]-8-[v1(44)]-16-[v2(1)]|",
+                       views: thumbnailImageView, userProfileImageView, seperatorView)
 
     }
     
@@ -67,6 +121,5 @@ extension UIView {
                                                         metrics: nil,
                                                         views: viewsDictionary)
         addConstraints(constraint)
-
     }
 }
